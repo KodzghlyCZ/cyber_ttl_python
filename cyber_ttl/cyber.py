145c29cd025bb0b2cd2984a66dab0e5e69d9e45e
Karel Matějovský <kodzghlyxd@gmail.com>
2024-12-13 20:14:31 +0100

    Added error code and sensor gas type mapping

--- cyber_ttl/cyber.py
+++ cyber_ttl/cyber.py
@@ -1,4 +1,5 @@
 from .modbus import readHoldingRegisters, readInputRegisters
+from .maps.error import error_map
 
 # Function to calculate gas concentration
 def calculate_concentration():
@@ -21,3 +22,35 @@ def calculate_concentration():
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+def get_gas_name(sensor_hex, gas_hex):
+   
+    # Read input registers (sensor reading)
+    reg_2000 = readHoldingRegisters(0x00, 1)[0]  # Offset 0x02 from 0x1000
+
+    # Read Real_FS scaling factor
+    reg_2007h = readHoldingRegisters(0x07, 1)[0] >> 8  # Offset 0x07 from 0x2000
+
+    
+    sensor_name = SENSOR_TYPE_MAP.get(sensor_hex, f"Unknown Sensor (0x{sensor_hex:X})")
+    gas_name = GAS_TYPE_MAP.get(gas_hex, f"Unknown Gas (0x{gas_hex:X})")
+    
+    return f"Sensor: {sensor_name}, Gas: {gas_name}"
+
+def get_error_state():
+    try:
+        # Read the error register (high byte of register 0x1003)
+        error_code = readHoldingRegisters(0x03, 1)[0] >> 8  # Offset 0x03 from 0x1000, and shift to get the high byte
+
+        # Get the error message from the error map, default to UNKNOWN_ERROR if not found
+        error_message = error_map.get(error_code, ErrorCode.UNKNOWN_ERROR).value
+
+        # Print the error code and corresponding message
+        print(f"Error code: {error_code}, Error message: {error_message}")
+
+        return error_message
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
---
+++ cyber_ttl/maps/error.py
@@ -0,0 +1,14 @@
+error_map = {
+            0x00: ErrorCode.NO_ERRORS,
+            0x01: ErrorCode.HEATING,
+            0x02: ErrorCode.E2PROM_ERROR,
+            0x03: ErrorCode.FLASH_ERROR,
+            0x04: ErrorCode.RAM_ERROR,
+            0x05: ErrorCode.VCC_ERROR,
+            0x08: ErrorCode.VGND_ERROR,
+            0x0A: ErrorCode.E2PROM_CKSM_ERROR,
+            0x0B: ErrorCode.CHANGE_SENSOR,
+            0x0C: ErrorCode.ANALOG_OUT_ERROR_4_20ma,
+            0x0D: ErrorCode.Heating_VGND_ERROR,
+            0x0E: ErrorCode.ADC_ERROR
+        }
