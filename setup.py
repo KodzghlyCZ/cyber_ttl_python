eb85f8662624ad134ccd461ee718651984ca1a14
Karel Matějovský <kodzghlyxd@gmail.com>
2025-03-09 15:33:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyber_ttl',
-    version='0.1.0',
+    version='0.2.0',
     description='A package for Modbus communication and gas concentration calculation',
     author='Karel Matějovský',
     author_email='karel@matejovsky.org',
